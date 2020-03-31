3dcd3e334c1c6416e4902feb6755058e72774fd9
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-03-31 23:17:11 +0300

    1

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>Home page
+            </h1>
+        </Fragment>
+
+    )
+}
