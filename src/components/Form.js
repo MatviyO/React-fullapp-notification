1814906e70c28dfa1477dbf544c68bf944bc609c
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-01 22:29:45 +0300

    3

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,18 @@
-import React from  'react'
+import React, {useState, useContext} from  'react'
+import {AlertContext} from "../context/alert/alertContext";
 
 export const Form = () => {
-    return (
-        <form action="">
-            <div className="form-group">
-                <input type="text"className="form-control" placeholder="Name" />
+    const [value,setValue] = useState('')
+    const alert = useContext(AlertContext)
 
+    const submitHandler = event => {
+        event.preventDefault()
+        alert.show(value, 'success')
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input type="text"className="form-control" placeholder="Name" value={value} onChange={e => setValue(e.target.value)} />
             </div>
         </form>
     )
